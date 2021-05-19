18c0255de3292634372310d9bbfb613a8e4df3b6
2D0 <lo7733ve@naver.com>
2021-05-19 17:44:08 +0900

    반응형 포트폴리오 시작

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-$('#mainSlider ul').bxSlider({
-  auto: true,
-  controls: false,
-  autoControls: false,
-  stopAutoOnClick: true,
-  pager: true,
+/* $('#diane').addClass('animate');
+
+
+$(function(){
+
+  var $container = $('.container');
+  var $offset = 300;
+  var $containerOST = $container.offset().top - $offset;
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() > $containerOST) {
+      $container.find('#diane').addClass('animate');
+    }
+  });
+
+
 });
+ */
+/* window.addEventListener('load', function(event){
+  AOS.init();
+}}; */
